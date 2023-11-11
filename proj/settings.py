a5065f3997bf718ccad833fbd295eb78dc343ca6
Abolarin <abolarin.david@lmu.edu.ng>
2023-11-11 12:29:16 +0100

    your message

--- proj/settings.py
+++ proj/settings.py
@@ -155,8 +155,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles_build/static/images")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {'default': {"BACKEND": 'cloudinary_storage.storage.MediaCloudinaryStorage'}, "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",}, }
 
 CLOUDINARY_STORAGE = {'CLOUD_NAME': env('CLOUD_NAME'), 'API_KEY': env('API_KEY'), 'API_SECRET': env('API_SECRET'), }
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
