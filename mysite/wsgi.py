a9b0dce8b187f140aedba3cd02ae149275eadef7
Abolarin <abolarin.david@lmu.edu.ng>
2023-11-11 16:45:40 +0100

    mm

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for prov project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prov.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
 
